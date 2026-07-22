d870f92c226552b4c9e7b5432506bf619707681b
xishuo-wang <3433949712@qq.com>
2026-07-22 15:29:17 +0800

    新增indices引入

--- src/mjlab/tasks/SQuRo_Slalom/mdp/__init__.py
+++ src/mjlab/tasks/SQuRo_Slalom/mdp/__init__.py
@@ -3,6 +3,7 @@ from mjlab.envs.mdp import *
 from .command import *
 from .curriculums import *
 from .events import *
+from .indices import *
 from .observations import *
 from .rewards import *
 from .terminations import *
