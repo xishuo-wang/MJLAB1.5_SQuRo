a0409fb5d835d11896215d7175321f835fa8b3cd
xishuo-wang <3433949712@qq.com>
2026-07-22 14:24:55 +0800

    适配SQuRo机器人引入

--- src/mjlab/asset_zoo/robots/__init__.py
+++ src/mjlab/asset_zoo/robots/__init__.py
@@ -16,3 +16,6 @@ from mjlab.asset_zoo.robots.unitree_go1.go1_constants import (
 from mjlab.asset_zoo.robots.unitree_go1.go1_constants import (
   get_go1_robot_cfg as get_go1_robot_cfg,
 )
+from mjlab.asset_zoo.robots.SQuRo.SQuRo_constants import (
+  get_squro_robot_cfg as get_squro_robot_cfg,
+)
