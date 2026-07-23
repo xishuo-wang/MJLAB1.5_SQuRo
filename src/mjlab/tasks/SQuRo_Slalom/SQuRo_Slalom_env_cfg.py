33cd142cbacf38a742c0dfab87119001e1cffa20
xishuo-wang <3433949712@qq.com>
2026-07-23 16:13:04 +0800

    调整格式

--- src/mjlab/tasks/SQuRo_Slalom/SQuRo_Slalom_env_cfg.py
+++ src/mjlab/tasks/SQuRo_Slalom/SQuRo_Slalom_env_cfg.py
@@ -71,10 +71,10 @@ def SQuRo_Slalom_Env_Cfg(play: bool = False) -> ManagerBasedRlEnvCfg:
         "mimic_vel": RewardTermCfg(func=mdp.compute_mimic_vel_reward, weight=1.0),
         "track_vel": RewardTermCfg(func=mdp.compute_vel_track_reward, weight=1.0),
         "track_omg": RewardTermCfg(func=mdp.compute_omg_track_reward, weight=1.0),
+        "track_path": RewardTermCfg(func=mdp.compute_path_track_reward, weight=1.0),
         "action_L1": RewardTermCfg(func=mdp.compute_action_L1_penalty, weight=1.0),
         "action_L2": RewardTermCfg(func=mdp.compute_action_L2_penalty, weight=1.0),
         "energy": RewardTermCfg(func=mdp.compute_energy_penalty, weight=1.0),
-        "path_track": RewardTermCfg(func=mdp.compute_path_track_reward, weight=1.0),
     }
 
     # 终止条件
