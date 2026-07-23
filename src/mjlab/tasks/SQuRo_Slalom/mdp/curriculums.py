76a842134fd72031fceee47e9aeb6cca40c062d6
xishuo-wang <3433949712@qq.com>
2026-07-23 18:23:55 +0800

    260723-5 调整权重

--- src/mjlab/tasks/SQuRo_Slalom/mdp/curriculums.py
+++ src/mjlab/tasks/SQuRo_Slalom/mdp/curriculums.py
@@ -7,9 +7,9 @@ _STAGES = (0, 1000, 2000)
 
 
 _CURVES: dict[str, tuple[float, ...]] = {
-    "weight_mimic_pos":         (4.0, 4.0, 4.0),
+    "weight_mimic_pos":         (5.0, 5.0, 5.0),
     "weight_mimic_vel":         (2.0, 2.0, 2.0),
-    "weight_track_vel":         (3.0, 3.0, 3.0),
+    "weight_track_vel":         (4.0, 4.0, 4.0),
     "weight_track_vyz":         (1.0, 1.0, 1.0),
     "weight_track_omg":         (5.0, 5.0, 5.0),
     "weight_track_path":        (10.0, 10.0, 10.0),
@@ -26,7 +26,7 @@ _CURVES: dict[str, tuple[float, ...]] = {
     "sigma_track_vel":          (50, 50, 50),
     "sigma_track_vyz":          (50, 50, 50),
     "sigma_track_omg":          (20, 20, 20),
-    "sigma_track_path":         (20, 20, 20),
+    "sigma_track_path":         (5, 5, 5),
 }
 
 
