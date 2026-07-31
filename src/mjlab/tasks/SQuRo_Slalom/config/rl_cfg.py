a28f8b3e7f0a885f8b2d0f7dbfc5e2774e36c56c
xishuo-wang <3433949712@qq.com>
2026-07-31 18:22:41 +0800

    260731-6 运行完整训练

--- src/mjlab/tasks/SQuRo_Slalom/config/rl_cfg.py
+++ src/mjlab/tasks/SQuRo_Slalom/config/rl_cfg.py
@@ -40,12 +40,12 @@ def SQuRo_Slalom_PPO_Runner_Cfg() -> RslRlOnPolicyRunnerCfg:
     experiment_name="SQuRo_Slalom",
     save_interval=100,
     num_steps_per_env=_STEPS_PER_ITER,
-    max_iterations=4_200,
+    max_iterations=8_000,
     
     clip_actions=3.14,
     seed=42,
     
-    resume=True,
-    load_run="2026-07-31_13-59-12",
-    load_checkpoint="model_3800.pt",
+    # resume=True,
+    # load_run="2026-07-31_13-59-12",
+    # load_checkpoint="model_3800.pt",
   )
