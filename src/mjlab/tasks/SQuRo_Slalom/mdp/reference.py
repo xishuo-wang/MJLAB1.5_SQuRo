5b58d561e9f948841ef3f3311add195cd37e2d22
xishuo-wang <3433949712@qq.com>
2026-08-01 16:49:56 +0800

    调整代码格式

--- src/mjlab/tasks/SQuRo_Slalom/mdp/reference.py
+++ src/mjlab/tasks/SQuRo_Slalom/mdp/reference.py
@@ -5,9 +5,9 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 from typing import TYPE_CHECKING
+from .path import get_path_curvature
 from .command import CURVATURE_TARGET_MAX
 from .indices import resolve_model_indices
-from .path import get_path_curvature, _RMIN as _SLALOM_RMIN
 if TYPE_CHECKING:
     from mjlab.envs.manager_based_rl_env import ManagerBasedRlEnv
 
