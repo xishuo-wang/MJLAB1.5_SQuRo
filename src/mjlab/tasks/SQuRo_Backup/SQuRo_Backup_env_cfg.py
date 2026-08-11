673df3675c7b5a0fb906d33e870bfa95ed8d78c3
xishuo-wang <3433949712@qq.com>
2026-08-11 11:59:18 +0800

    260811-2 启用平滑性惩罚

--- src/mjlab/tasks/SQuRo_Backup/SQuRo_Backup_env_cfg.py
+++ src/mjlab/tasks/SQuRo_Backup/SQuRo_Backup_env_cfg.py
@@ -74,10 +74,9 @@ def SQuRo_Backup_Env_Cfg(play: bool = False) -> ManagerBasedRlEnvCfg:
         # "stand_still": RewardTermCfg(func=mdp.compute_stand_still_penalty, weight=0.0),
         # "fallen": RewardTermCfg(func=mdp.compute_fallen_penalty, weight=1.0),
         # "corridor": RewardTermCfg(func=mdp.compute_corridor_reward, weight=0.0),
-        # mimic/height 测试阶段: 平滑/能耗惩罚会抑制腿部大动作(站立角->支撑位), 权重置 0
-        "action_L1": RewardTermCfg(func=mdp.compute_action_L1_penalty, weight=0.0),
-        "action_L2": RewardTermCfg(func=mdp.compute_action_L2_penalty, weight=0.0),
-        "energy": RewardTermCfg(func=mdp.compute_energy_penalty, weight=0.0),
+        "action_L1": RewardTermCfg(func=mdp.compute_action_L1_penalty, weight=1.0),
+        "action_L2": RewardTermCfg(func=mdp.compute_action_L2_penalty, weight=1.0),
+        "energy": RewardTermCfg(func=mdp.compute_energy_penalty, weight=1.0),
     }
 
     # 终止条件
