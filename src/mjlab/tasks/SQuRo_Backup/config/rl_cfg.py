51e0405d2a08bb8d08c77dcfd6bf9ed09b9b9292
xishuo-wang <3433949712@qq.com>
2026-09-15 15:14:58 +0800

    260915 初始化

--- src/mjlab/tasks/SQuRo_Backup/config/rl_cfg.py
+++ src/mjlab/tasks/SQuRo_Backup/config/rl_cfg.py
@@ -28,7 +28,6 @@ def SQuRo_Backup_PPO_Runner_Cfg() -> RslRlOnPolicyRunnerCfg:
             use_clipped_value_loss=True,
             clip_param=0.2,
             entropy_coef=0.01,
-            # step_dt 0.02->0.005 后 rollout 物理时间变短, 增大 epochs/minibatches 提高数据利用率
             num_learning_epochs=5,
             num_mini_batches=4,
             learning_rate=3.0e-4,
@@ -41,7 +40,7 @@ def SQuRo_Backup_PPO_Runner_Cfg() -> RslRlOnPolicyRunnerCfg:
         experiment_name="SQuRo_Backup",
         save_interval=100,
         num_steps_per_env=_STEPS_PER_ITER,
-        max_iterations=1_000,
+        max_iterations=3_000,
 
         clip_actions=6.0,
         seed=42,
